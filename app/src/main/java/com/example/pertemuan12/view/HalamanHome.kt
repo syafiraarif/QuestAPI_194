a32496727dc28eeb11093a7f5c9cc27a75198820
syairaarif <putrisyafira4444@gmail.com>
2025-12-19 13:49:13 +0700

    menambahkan fungsi loadingscreen dimana ini digunakan ketika aplikasi mengeload data dia bakal nampilin gambar loading

--- app/src/main/java/com/example/pertemuan12/view/HalamanHome.kt
+++ app/src/main/java/com/example/pertemuan12/view/HalamanHome.kt
@@ -1,8 +1,10 @@
 package com.example.pertemuan12.view
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -16,7 +18,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pertemuan12.viewmodel.HomeViewModel
 import com.example.pertemuan12.viewmodel.provider.PenyediaViewModel
@@ -86,4 +90,15 @@ fun HomeBody(
             )
         }
     }
+}
+
+@Composable
+fun LoadingScreen(
+    modifier: Modifier = Modifier
+){
+    Image(
+        modifier = modifier.size(200.dp),
+        painter = painterResource(R.drawable.loading_img),
+        contentDescription = stringResource(R.string.loading)
+    )
 }
