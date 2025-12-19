f1285ecda345ecbe501154cda0481161433d5827
syairaarif <putrisyafira4444@gmail.com>
2025-12-19 14:04:22 +0700

    membuat alamat tujuan untuk halaman form input

---
+++ app/src/main/java/com/example/pertemuan12/view/route/DestinasiEntry.kt
@@ -0,0 +1,9 @@
+package com.example.pertemuan12.view.route
+
+import com.example.pertemuan12.R
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+
+}
