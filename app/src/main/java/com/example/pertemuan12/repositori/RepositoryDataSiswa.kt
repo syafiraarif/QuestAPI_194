08766225ff993f75922d758fdeb0d036d9026230
syairaarif <putrisyafira4444@gmail.com>
2025-12-19 10:24:23 +0700

    menambahkan di repositori, fungsi jaringanrepositoridatasiswa ini digunakan untuk memberi tahu bahwa aplikasi ini menggunakan jaringan dan untuk supaya ui bisa nambah dan baca data dari api

--- app/src/main/java/com/example/pertemuan12/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/pertemuan12/repositori/RepositoryDataSiswa.kt
@@ -1,10 +1,18 @@
 package com.example.pertemuan12.repositori
 
+import com.example.pertemuan12.apiservice.ServiceApiSiswa
 import com.example.pertemuan12.modedata.DataSiswa
 
 interface RepositoryDataSiswa{
     suspend fun getDataSiswa(): List<DataSiswa>
 
     suspend fun postDataSiswa(dataSiswa: DataSiswa) :retrofit2.Response<Void>
+}
+
+class JaringanRepositoryDataSiswa(
+    private val serviceApiSiswa: ServiceApiSiswa
+) : RepositoryDataSiswa{
+    override suspend fun getDataSiswa() : List<DataSiswa> = serviceApiSiswa.getSiswa()
+    override suspend fun postDataSiswa(dataSiswa: DataSiswa) :retrofit2.Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
 }
 
