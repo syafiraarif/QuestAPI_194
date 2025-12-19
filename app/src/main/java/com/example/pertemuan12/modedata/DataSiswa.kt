e5ae4bcae91ccff37a02d424d7c243e3acbe193d
syairaarif <putrisyafira4444@gmail.com>
2025-12-19 10:13:25 +0700

    menambahkan fungsi datasiswa yang digunakan untuk mengubah data yang ada di backend menjadi data yang ada di frontend, dan fungsi data siswa todetailsiswa itu fungsinya mengisi form dengan data yang sudah ada

--- app/src/main/java/com/example/pertemuan12/modedata/DataSiswa.kt
+++ app/src/main/java/com/example/pertemuan12/modedata/DataSiswa.kt
@@ -29,6 +29,18 @@ fun DetailSiswa.toDataSiswa(): DataSiswa = DataSiswa(
     telpon = telpon
 )
 
+fun DataSiswa.toUiStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateSiswa(
+    detailSiswa = this.toDetailSiswa(),
+    isEntryValid = isEntryValid
+)
+
+fun DataSiswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
+)
+
 
 
 
