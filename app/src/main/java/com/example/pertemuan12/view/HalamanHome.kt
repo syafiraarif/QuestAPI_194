0d601dacdb114e883c10c128fea6e9b36b419029
syairaarif <putrisyafira4444@gmail.com>
2025-12-19 13:53:02 +0700

    fungsi daftar siswa ini digunakan untuk menampilkan list data siswa, dan fungsi item siswa digunakan untuk membuat bahwa satu data siswa nantinya akan ada di dalam satu card

--- app/src/main/java/com/example/pertemuan12/view/HalamanHome.kt
+++ app/src/main/java/com/example/pertemuan12/view/HalamanHome.kt
@@ -1,14 +1,23 @@
 package com.example.pertemuan12.view
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -28,6 +37,7 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pertemuan12.viewmodel.HomeViewModel
 import com.example.pertemuan12.viewmodel.provider.PenyediaViewModel
 import com.example.pertemuan12.R
+import com.example.pertemuan12.modedata.DataSiswa
 import com.example.pertemuan12.viewmodel.StatusUiSiswa
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -117,6 +127,57 @@ fun ErrorScreen(retryAction: ()-> Unit, modifier: Modifier = Modifier){
             .padding(16.dp))
         Button(onClick = retryAction) {
             Text(stringResource(R.string.retry))
+        }
+    }
+}
+
+@Composable
+fun DaftarSiswa(
+    itemSiswa: List<DataSiswa>,
+    onSiswaClick:(DataSiswa) -> Unit,
+    modifier: Modifier = Modifier
+){
+    LazyColumn(modifier = Modifier) {
+        items(items = itemSiswa, key = {it.id}){
+                person ->
+            ItemSiswa(
+                siswa = person,
+                modifier = Modifier
+                    .padding(dimensionResource(R.dimen.padding_small))
+                    .clickable{onSiswaClick(person)}
+            )
+        }
+    }
+}
+
+@Composable
+fun ItemSiswa(
+    siswa: DataSiswa,
+    modifier: Modifier = Modifier
+){
+    Card(modifier = modifier,
+        elevation = CardDefaults.cardElevation(2.dp)) {
+        Column(modifier = Modifier.padding(dimensionResource(R.dimen.padding_large)),
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small))
+        ) {
+            Row(modifier = Modifier.fillMaxWidth()) {
+                Text(
+                    text = siswa.nama,
+                    style = MaterialTheme.typography.titleLarge
+                )
+                Spacer(modifier.weight(1f))
+                Icon(
+                    imageVector = Icons.Default.Phone,contentDescription = null
+                )
+                Text(
+                    text = siswa.telpon,
+                    style = MaterialTheme.typography.titleMedium
+                )
+            }
+            Text(
+                text = siswa.alamat,
+                style = MaterialTheme.typography.titleMedium
+            )
         }
     }
 }
