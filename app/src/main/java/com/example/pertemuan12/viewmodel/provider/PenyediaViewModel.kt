96f20f47c724d576304644fe0eac7ca6e94d9c61
syairaarif <putrisyafira4444@gmail.com>
2025-12-19 13:12:25 +0700

    penyedia viw model digunakan untuk memanggil atau menginisialisasi semua view model sehingga semua viw model cukup di akses lewat file ini

---
+++ app/src/main/java/com/example/pertemuan12/viewmodel/provider/PenyediaViewModel.kt
@@ -0,0 +1,23 @@
+package com.example.pertemuan12.viewmodel.provider
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.pertemuan12.repositori.AplikasiDataSiswa
+import com.example.pertemuan12.viewmodel.EntryViewModel
+import com.example.pertemuan12.viewmodel.HomeViewModel
+
+fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa = (
+        this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiDataSiswa
+        )
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(aplikasiDataSiswa().containerApp.repositoryDataSiswa)
+        }
+        initializer {
+            EntryViewModel(aplikasiDataSiswa().containerApp.repositoryDataSiswa)
+        }
+    }
+}
