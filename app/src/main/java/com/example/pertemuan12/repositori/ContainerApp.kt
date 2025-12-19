b5fb2d9559a3ff03e33ca8c3577505dc6f699041
syairaarif <putrisyafira4444@gmail.com>
2025-12-19 10:32:18 +0700

    menambahkan class defaultcontainerapp dimana ini isinya tentang depedensies yang bakal digunain biar bisa komunikasi ke server

--- app/src/main/java/com/example/pertemuan12/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/pertemuan12/repositori/ContainerApp.kt
@@ -1,8 +1,50 @@
 package com.example.pertemuan12.repositori
 
+import android.app.Application
+import com.example.pertemuan12.apiservice.ServiceApiSiswa
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
 
 
 interface ContainerApp{
     val repositoryDataSiswa: RepositoryDataSiswa
 }
+
+class DefaultContainerApp : ContainerApp{
+    private val baseurl = "http://10.0.2.2/tiumy/"
+
+    val logging = HttpLoggingInterceptor().apply {
+        level= HttpLoggingInterceptor.Level.BODY
+
+    }
+
+    val klien = OkHttpClient.Builder()
+        .addInterceptor(logging)
+        .build()
+
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl(baseurl)
+        .addConverterFactory(
+            Json {
+                ignoreUnknownKeys = true
+                prettyPrint = true
+                isLenient = true
+            }.asConverterFactory("application/json".toMediaType())
+        )
+        .client(klien)
+        .build()
+
+    private val retrofitService: ServiceApiSiswa by lazy {
+        retrofit.create(ServiceApiSiswa::class.java)
+    }
+
+    override val repositoryDataSiswa: RepositoryDataSiswa by lazy {
+        JaringanRepositoryDataSiswa(retrofitService) }
+}
+
+
 
