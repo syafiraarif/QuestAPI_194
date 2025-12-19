d6c1408ca1e15bb97bf43ba30f1a23b6c58b3831
syairaarif <putrisyafira4444@gmail.com>
2025-12-19 10:11:34 +0700

    menambahkan data class detail siswa yang digunakan untuk menerima inputan data siswa sebelum di kirim ke layar, fun detail siswa berfungsi untuk mengubah data yang ada di data class detail siswa menjadi data yang akan siap ditampilkan di ui

--- app/src/main/java/com/example/pertemuan12/modedata/DataSiswa.kt
+++ app/src/main/java/com/example/pertemuan12/modedata/DataSiswa.kt
@@ -15,3 +15,21 @@ data class UIStateSiswa(
     val isEntryValid: Boolean = false
 )
 
+data class DetailSiswa(
+    val id: Int = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String = "",
+)
+
+fun DetailSiswa.toDataSiswa(): DataSiswa = DataSiswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
+)
+
+
+
+
+
