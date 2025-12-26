39ee55b73d89c7646ff884e3ebca1fbdba6063b5
syairaarif <putrisyafira4444@gmail.com>
2025-12-26 11:36:23 +0700

    membuat destinasi untuk edit

---
+++ app/src/main/java/com/example/pertemuan12/view/route/DestinasiEdit.kt
@@ -0,0 +1,11 @@
+package com.example.pertemuan12.view.route
+
+import com.example.pertemuan12.R
+
+object DestinasiEdit : DestinasiNavigasi {
+    override val route = "item_edit"
+    override val titleRes = R.string.edit_siswa
+
+    const val itemIdArg = "idSiswa"
+    val routeWithArg = "$route/{$itemIdArg}"
+}
