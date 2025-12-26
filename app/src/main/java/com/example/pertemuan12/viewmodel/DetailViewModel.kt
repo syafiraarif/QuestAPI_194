20d8f72e738cc61a834573580d09fc791521dfc1
syairaarif <putrisyafira4444@gmail.com>
2025-12-26 11:08:08 +0700

    membuat class DetailViewModel yang memliki 2 paramter

--- app/src/main/java/com/example/pertemuan12/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/pertemuan12/viewmodel/DetailViewModel.kt
@@ -1,9 +1,16 @@
 package com.example.pertemuan12.viewmodel
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
 import com.example.pertemuan12.modedata.DataSiswa
+import com.example.pertemuan12.repositori.RepositoryDataSiswa
 
 sealed interface StatusUIDetail {
     data class Success (val satusiswa: DataSiswa) : StatusUIDetail
     object Error : StatusUIDetail
     object Loading : StatusUIDetail
+}
+
+class DetailViewModel(savedStateHandle: SavedStateHandle, private val repositoryDataSiswa: RepositoryDataSiswa): ViewModel() {
+
 }
