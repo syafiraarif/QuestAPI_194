2e8502bf831963ec055950d31af1bae69a6f59e5
syairaarif <putrisyafira4444@gmail.com>
2025-12-26 10:40:52 +0700

    menyimpan state ui siswa, menyimpan data untuk ui nya

--- app/src/main/java/com/example/pertemuan12/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/pertemuan12/viewmodel/EditViewModel.kt
@@ -1,10 +1,17 @@
 package com.example.pertemuan12.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableStateSetOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import com.example.pertemuan12.modedata.UIStateSiswa
 import com.example.pertemuan12.repositori.RepositoryDataSiswa
 
 class EditViewModel (savedStateHandle: SavedStateHandle,private val repositoryDataSiswa:
     RepositoryDataSiswa): ViewModel(){
 
+        var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
 }
