671df626ff5c8436c0cbe25c16f42b72650bc420
syairaarif <putrisyafira4444@gmail.com>
2025-12-26 10:46:05 +0700

    mengambil idSiswa dari savestatehandle, nanti data detail siswa akan di muat dan di tampilkan di tampilan UI

--- app/src/main/java/com/example/pertemuan12/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/pertemuan12/viewmodel/EditViewModel.kt
@@ -6,12 +6,23 @@ import androidx.compose.runtime.mutableStateSetOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.pertemuan12.modedata.UIStateSiswa
+import com.example.pertemuan12.modedata.toUiStateSiswa
 import com.example.pertemuan12.repositori.RepositoryDataSiswa
+import kotlinx.coroutines.launch
 
 class EditViewModel (savedStateHandle: SavedStateHandle,private val repositoryDataSiswa:
     RepositoryDataSiswa): ViewModel(){
 
         var uiStateSiswa by mutableStateOf(UIStateSiswa())
         private set
+
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+    init {
+        viewModelScope.launch {
+            uiStateSiswa = repositoryDataSiswa.getSatuSiswa(idSiswa)
+                .toUiStateSiswa(true)
+        }
+    }
 }
