3be38486dc4722780f235858dc3f4183b9125bd2
syairaarif <putrisyafira4444@gmail.com>
2025-12-26 12:06:38 +0700

    Menambahkan komponen barisdetaildata

--- app/src/main/java/com/example/pertemuan12/view/HalamanDetail.kt
+++ app/src/main/java/com/example/pertemuan12/view/HalamanDetail.kt
@@ -169,3 +169,14 @@ fun DetailDataSiswa(
     } }
 }
 
+@Composable
+private fun BarisDetailData(
+    @StringRes labelResID: Int, itemDetail: String, modifier: Modifier = Modifier
+){
+    Row(modifier = modifier) {
+        Text(stringResource(labelResID))
+        Spacer(modifier = Modifier.weight(1f))
+        Text(itemDetail, fontWeight = FontWeight.Bold)
+    }
+}
+
