f547cc44c6b28fb6e2bf5d43b2007db3902fcf37
syairaarif <putrisyafira4444@gmail.com>
2025-12-26 10:09:06 +0700

    menambahkan editSatuSiswa pada RepositoryDataSiswa untuk menjadi jembatan antara viewmodel dengan api

--- app/src/main/java/com/example/pertemuan12/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/pertemuan12/repositori/RepositoryDataSiswa.kt
@@ -8,7 +8,10 @@ interface RepositoryDataSiswa{
 
     suspend fun postDataSiswa(dataSiswa: DataSiswa) :retrofit2.Response<Void>
 
-    suspend fun getSatuSiswa(id:Int) : DataSiswa
+    suspend fun getSatuSiswa(id:Int,datasiswa: DataSiswa) : DataSiswa
+
+    suspend fun editSatuSiswa (id:Int) : retrofit2.Response<Void>
+
 }
 
 class JaringanRepositoryDataSiswa(
