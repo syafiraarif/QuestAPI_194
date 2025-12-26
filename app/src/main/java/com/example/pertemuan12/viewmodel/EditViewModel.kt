e751f553d89f2844c656ba12e5861d5ee00438f3
syairaarif <putrisyafira4444@gmail.com>
2025-12-26 10:22:13 +0700

    membuat class EditViewModel yang memiliki 2 parameter

---
+++ app/src/main/java/com/example/pertemuan12/viewmodel/EditViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.pertemuan12.viewmodel
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.pertemuan12.repositori.RepositoryDataSiswa
+
+class EditViewModel (savedStateHandle: SavedStateHandle,private val repositoryDataSiswa:
+    RepositoryDataSiswa): ViewModel(){
+
+}
