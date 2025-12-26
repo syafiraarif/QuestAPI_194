226d197e9ca6d9f02f0413b9b664dce7cfac0805
syairaarif <putrisyafira4444@gmail.com>
2025-12-26 10:16:31 +0700

    menambahkan hapusSatuSiswa pada JaringanRepositoryDataSiswa untuk menjadi jembatan antara repositori dengan api

--- app/src/main/java/com/example/pertemuan12/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/pertemuan12/repositori/RepositoryDataSiswa.kt
@@ -25,6 +25,8 @@ class JaringanRepositoryDataSiswa(
     override suspend fun getSatuSiswa(id: Int): DataSiswa = serviceApiSiswa.getSatuSiswa(id)
     override suspend fun editSatuSiswa(id: Int, dataSiswa: DataSiswa): retrofit2.Response<Void> =
         serviceApiSiswa.editSatuSiswa(id, dataSiswa)
+    override suspend fun hapusSatuSiswa(id: Int): retrofit2.Response<Void> =
+        serviceApiSiswa.hapusSatuSiswa(id)
 
 }
 
